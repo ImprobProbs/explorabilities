58519b66c0d6df0f09a37b9b64dbe3bd344fbe09
Derek Young <dthomasy@gmail.com>
2017-01-26 15:31:13 -0800

    Update map width

--- client/components/MapContainer.jsx
+++ client/components/MapContainer.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 export default class MapContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       query: 'Cancun',
-      key: 'AIzaSyCBb0bm-_wNIf3oDMi-5PN_zeOf1bRWstI'
     };
   }
 
@@ -19,28 +18,11 @@ export default class MapContainer extends React.Component {
         </form>
         <div id="googleMaps">
           <iframe id="mapEmbed" frameBorder="0" style={{border: 10}}
-            src={`https://www.google.com/maps/embed/v1/search?key=${this.state.key}&q=point_of_interest+near+${this.state.query}`}>
-
+            src={`https://www.google.com/maps/embed/v1/search?key=AIzaSyCBb0bm-_wNIf3oDMi-5PN_zeOf1bRWstI&q=point_of_interest+near+${this.state.query}`}>
           </iframe>
         </div>
       </div>
     );
-  }
-
-  componentWillMount () {
-    // this.getKey();
-  }
-
-  getKey () {
-    axios.get('api/google/key')
-    .then(function(response){
-      //AXIOS GET THE MAP
-      //SOME CALLBACK(response)
-      console.log(response.data);
-      console.log(response.status);
-    }).catch(function(error) {
-      console.log(error);
-    });
   }
 
   updateQuery(e) {
@@ -48,4 +30,4 @@ export default class MapContainer extends React.Component {
       query: e.target.value
     });
   }
-};
+}
