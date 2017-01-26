4bdd4cc4ddd244b34b0281a818977229155522cd
Derek Young <dthomasy@gmail.com>
2017-01-25 19:50:58 -0800

    Add google api key

--- client/app.jsx
+++ client/app.jsx
@@ -15,7 +15,7 @@ export default class App extends React.Component {
         <Search />
         <div id="googleMaps">
           <iframe width="600" height="450" frameBorder="0" style={{border: 0}}
-          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJIQBpAG2ahYAR_6128GcTUEo&key=AIzaSyCBb0bm-_wNIf3oDMi-5PN_zeOf1bRWstI" allowFullScreen></iframe>
+          src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJIQBpAG2ahYAR_6128GcTUEo&key=${window.GOOGlE_API_KEY}`} allowFullScreen></iframe>
         </div>
         <ItineraryList list = {[1,2,3]}/>
       </div>
