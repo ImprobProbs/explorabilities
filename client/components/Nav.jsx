6935d02c9459b15e0212a90a83ccfebc7675598c
David Chang <dchang103@gmail.com>
2017-01-26 12:28:36 -0800

    navbar tweak, background css tweak

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 const Nav = () => (
   <div id='nav'>
-    <div id="searchBar">
-      <input id="searchForm" type="text" />
-      <button id="exploreButton">Explore!</button>
-    </div> 
+    <input id="searchForm" type="text" />
+    <button id="exploreButton">Explore!</button>
     <button className="btn" id="logout">log out</button>
   </div>
 )
