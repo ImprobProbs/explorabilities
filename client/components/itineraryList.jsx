9241673cf1751d958abc7dcbf6897a1ef85f77d7
Derek Young <dthomasy@gmail.com>
2017-01-26 11:19:39 -0800

    Place and itinerary holders

--- client/components/itineraryList.jsx
+++ client/components/itineraryList.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const ItineraryList = (props)  => (
   <ul id="itinerary">
     {props.list.map(function(listValue){
-      return <li className= "itineraryItem" key={listValue}>{listValue}</li>;
+      // return <li className= "itineraryItem" key={listValue}>{listValue}</li>;
       return (
-        <li key={listValue}>
+        <li key={listValue.city}>
          <a href ="#">City: {listValue.city}</a>
          <a href ="#">State: {listValue.state}</a>
          <a href ="#">Things To Eat: {listValue.thingsToEat}</a>
@@ -18,20 +18,3 @@ const ItineraryList = (props)  => (
 
 
 export default ItineraryList;
-
-
-// export default class itineraryList extends React.createClass {
-//   constructor (props) {
-//     super(props);
-//   }
-
-//   render () {
-//     return (
-//       <ul>
-//         {this.props.list.map(function(listValue){
-//           return <li>{listValue}</li>;
-//         })}
-//       </ul>
-//     );
-//   }
-// };
