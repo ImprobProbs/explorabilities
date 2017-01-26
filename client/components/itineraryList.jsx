975166275ebfb7a31b5c0adc4f0cbbefbc54d9ec
Tenzin Sonam <tenso2006@gmail.com>
2017-01-26 12:38:16 -0800

    merged

--- client/components/itineraryList.jsx
+++ client/components/itineraryList.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
 const ItineraryList = (props)  => (
-  <tr id="itinerary">
-    {props.list.map(function(listValue, index){
-      return (
-        <td key={index}>
-         <a href ="#">City: {listValue.city}</a>
-         <a href ="#">State: {listValue.state}</a>
-         <a href ="#">Things To Eat: {listValue.thingsToEat}</a>
-         <a href ="#">Hot Spot: {listValue.hotSpot}</a>
-        </td>
-      );
-    })}
-  </tr>
+  <div id="itinerary">
+    <h3 className="itineraryHeader">Itinerary List</h3>
+    <ul>
+      {props.list.map(function(listValue, index){
+        return (
+          <li key={index} className="itineraryItem">
+            <div>
+               <label>City:</label><a href ="#">{listValue.city}</a><br/>
+               <label>State:</label><a href ="#">{listValue.state}</a><br/>
+               <label>Things To Eat: </label><a href ="#">{listValue.thingsToEat}</a><br/>
+               <label>Hot Spot: </label><a href ="#">{listValue.hotSpot}</a>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
 );
-
 
 export default ItineraryList;
