3451d2360d863f784c84e09773e9cddb2b323c55
David Chang <dchang103@gmail.com>
2017-01-25 16:02:28 -0800

    added components, css update

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import reactDOM from 'react-dom';
+import Search from '../public/components/Search.jsx';
 
 export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <p>okay</p>
+    return (
+      <Search />
+      );
   }
 }
