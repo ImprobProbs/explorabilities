5506cd1ad88194e7c84f33f0ab4565cc74128277
Derek Young <dthomasy@gmail.com>
2017-01-25 16:23:06 -0800

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 const chai = require('chai');
+const request = require('request');
 
 describe('Database functionality', function() {
   describe('Users', function() {
     it('It should create a user on a post request to /users/create', function() {
-      // expect(true).to.equal(true);
+      chai.expect(true).to.equal(true);
     });
   });
 });
