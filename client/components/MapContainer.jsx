18de1a4c3b49686660abd8ad0ace01137c245aed
David Chang <dchang103@gmail.com>
2017-01-26 17:41:27 -0800

    nav bar buttons

--- client/components/MapContainer.jsx
+++ client/components/MapContainer.jsx
@@ -14,15 +14,19 @@ export default class MapContainer extends React.Component {
       <div>
         <form>
           <input id="searchForm" type="text" value={this.state.query} onChange={this.updateQuery.bind(this)} />
-          <button type="submit" id="exploreButton">Explore!</button>
         </form>
         <div id="googleMaps">
-          <iframe id="mapEmbed" frameBorder="0" style={{border: 10}}
+          <iframe id="mapEmbed" frameBorder="0" style={{border: 10}} onClick={this.test}
             src={`https://www.google.com/maps/embed/v1/search?key=AIzaSyCBb0bm-_wNIf3oDMi-5PN_zeOf1bRWstI&q=point_of_interest+near+${this.state.query}`}>
+          }
           </iframe>
         </div>
       </div>
     );
+  }
+
+  test(e) {
+    console.log(e.target);
   }
 
   updateQuery(e) {
