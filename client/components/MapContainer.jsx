9657aab508d51491903c54d78e747beb18f200e8
Derek Young <dthomasy@gmail.com>
2017-01-30 12:13:36 -0800

    Map responds to clicks on places

--- client/components/MapContainer.jsx
+++ client/components/MapContainer.jsx
@@ -5,7 +5,7 @@ export default class MapContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: 'Cancun',
+      query: 'Cancun'
     };
   }
 
@@ -13,21 +13,158 @@ export default class MapContainer extends React.Component {
     return (
       <div>
         <form>
-          <input id="searchForm" type="text" value={this.state.query} onChange={this.updateQuery.bind(this)} />
+          <input
+            id="searchForm"
+            type="text"
+            placeholder="Enter Your Destination (E.g. Cancun, Mexico)"
+            onChange={this.updateQuery.bind(this)}
+          />
           <button type="submit" id="exploreButton">Explore!</button>
         </form>
-        <div id="googleMaps">
-          <iframe id="mapEmbed" frameBorder="0" style={{border: 10}}
-            src={`https://www.google.com/maps/embed/v1/search?key=AIzaSyCBb0bm-_wNIf3oDMi-5PN_zeOf1bRWstI&q=point_of_interest+near+${this.state.query}`}>
-          </iframe>
-        </div>
+        <div id="googleMaps"></div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.createMap();
   }
 
   updateQuery(e) {
     this.setState({
       query: e.target.value
     });
+  }
+
+  createMap() {
+    const context = this;
+
+    /* ########## JSONP call for Google Map data ########## */
+    (function fetchMap() {
+      window.initMap = initMap;
+      const ref = window.document.getElementsByTagName('script')[0];
+      const script = window.document.createElement('script');
+      script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyCBb0bm-_wNIf3oDMi-5PN_zeOf1bRWstI&libraries=places&callback=initMap';
+      ref.parentNode.insertBefore(script, ref);
+      script.onload = function () {
+        this.remove();
+      };
+    })();
+
+    /* ################### Map Init ################### */
+    let map, places, autocomplete;
+    let markers = [];
+
+    function initMap() {
+      const sanFrancisco = {lat: 37.775, lng: -122.42};
+
+      map = new google.maps.Map(document.getElementById('googleMaps'), {
+        center: sanFrancisco,
+        zoom: 8
+      });
+
+      autocomplete = new google.maps.places.Autocomplete((
+          document.getElementById('searchForm')), {
+            types: ['(cities)']
+          });
+
+      places = new google.maps.places.PlacesService(map);
+
+      autocomplete.addListener('place_changed', onPlaceChanged);
+    }
+
+    // When the user selects a city, get the place details for the city and
+    // zoom the map in on the city.
+    function onPlaceChanged() {
+      const place = autocomplete.getPlace();
+      if (place.geometry) {
+        map.panTo(place.geometry.location);
+        map.setZoom(15);
+        search();
+      } else {
+        document.getElementById('autocomplete').placeholder = 'Enter a city';
+      }
+    }
+
+    // Search for attractions in the selected city, within the viewport of the map.
+    function search() {
+      const search = {
+        bounds: map.getBounds(),
+        types: [
+          'amusement_park',
+          'aquarium',
+          'art_gallery',
+          'bar',
+          'book_store',
+          'bowling_alley',
+          'cafe',
+          'campground',
+          'casino',
+          'library',
+          'lodging',
+          'movie_theater',
+          'museum',
+          'night_club',
+          'park',
+          'restaurant',
+          'spa',
+          'stadium',
+          'zoo'
+        ]
+      };
+
+      places.nearbySearch(search, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          clearMarkers();
+
+          // Create a marker for each item found
+          for (var i = 0; i < results.length; i++) {
+            let iconImage = {
+              url: results[i].icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+            // Use marker animation to drop the icons incrementally on the map.
+            markers[i] = new google.maps.Marker({
+              position: results[i].geometry.location,
+              animation: google.maps.Animation.DROP,
+              icon: iconImage
+            });
+            // If the user clicks a marker, call setPlace to update the object in the Place component.
+            markers[i].placeResult = results[i];
+            google.maps.event.addListener(markers[i], 'click', setPlace);
+            setTimeout(dropMarker(i), i * 100);
+          }
+        }
+      });
+    }
+
+    function clearMarkers() {
+      for (var i = 0; i < markers.length; i++) {
+        if (markers[i]) {
+          markers[i].setMap(null);
+        }
+      }
+      markers = [];
+    }
+
+    function dropMarker(i) {
+      return function() {
+        markers[i].setMap(map);
+      };
+    }
+
+    function setPlace() {
+      const marker = this;
+      places.getDetails({placeId: marker.placeResult.place_id},
+        function(place, status) {
+          if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            return;
+          }
+          context.props.updatePlace(place);
+        });
+    }
   }
 }
--- client/components/Place.jsx
+++ client/components/Place.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Place extends React.Component {
+export default class Place extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -8,16 +8,29 @@ class Place extends React.Component {
   render() {
     return (
       <div id='place'>
-        <img src="img/833.jpg" height="100" width="100"/>
-        <ul>
-          <li>More place info</li>
-          <li>More place info</li>
-          <li>More place info</li>
-          <li>More place info</li>
-        </ul>
+        <table>
+          <tr id="iw-url-row" className="iw_table_row">
+            <td id="iw-icon" className="iw_table_icon"></td>
+            <td id="iw-url"></td>
+          </tr>
+          <tr id="iw-address-row" className="iw_table_row">
+            <td className="iw_attribute_name">Address:</td>
+            <td id="iw-address"></td>
+          </tr>
+          <tr id="iw-phone-row" className="iw_table_row">
+            <td className="iw_attribute_name">Telephone:</td>
+            <td id="iw-phone"></td>
+          </tr>
+          <tr id="iw-rating-row" className="iw_table_row">
+            <td className="iw_attribute_name">Rating:</td>
+            <td id="iw-rating"></td>
+          </tr>
+          <tr id="iw-website-row" className="iw_table_row">
+            <td className="iw_attribute_name">Website:</td>
+            <td id="iw-website"></td>
+          </tr>
+        </table>
       </div>
-    )
+    );
   }
 }
-
-export default Place
