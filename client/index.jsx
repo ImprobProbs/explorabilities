560b28168906b40798b7dd2ccf1be755c2c6baa5
Derek Young <dthomasy@gmail.com>
2017-02-03 16:33:02 -0800

    Web token authentication

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './app.jsx';
 import Auth from './auth/auth.jsx';
+import authHelpers from './auth/auth-helpers.js';
 import Signin from './auth/signin.jsx';
 import Signup from './auth/signup.jsx';
 import Explore from './components/Explore.jsx';
@@ -15,7 +16,18 @@ ReactDOM.render(
       <Route path="/auth/signup" component={Signup}/>
     </Route>
     <Route path="/" component={App}>
-      <Route path="/explore" component={Explore}/>
-      <Route path="/myplaces" component={MyPlaces}/>
+      <Route path="/explore" component={Explore} onEnter={requireAuth}/>
+      <Route path="/myplaces" component={MyPlaces} onEnter={requireAuth}/>
     </Route>
   </Router>, document.getElementById('app'));
+
+function requireAuth(nextState, replace) {
+  if (!authHelpers.loggedIn()) {
+    replace({
+      pathname: '/auth/signin',
+      state: {
+        nextPathname: nextState.location.pathname
+      }
+    });
+  }
+}
