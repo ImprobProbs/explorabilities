8595cc58594af1d1ff2062a5b07213c5316e1540
Tenzin Sonam <tenso2006@gmail.com>
2017-02-03 23:11:14 -0800

    rating change to icon

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import StarRatingComponent from 'react-star-rating-component';
 
 const Review = (props) => {
   console.log(props);
+  const { rating } = props.rating;
   return (
     <div id="place-address">
       <em>{props.reviewer}</em><br/>
-      <cite>Rating: {props.rating}</cite>
+      <h4>Rating from state: {rating}</h4>
+      <StarRatingComponent
+        name="rate2"
+        editing={false}
+        renderStarIcon={() => <span></span>}
+        starCount={5}
+        value={props.rating}
+      />
       <article>{props.review}</article>
       <em>{props.date}</em>
       <br/><br/>
