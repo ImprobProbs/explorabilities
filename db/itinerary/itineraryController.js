5b8b823719571a87f24477631270cca76bcd5944
Derek Young <dthomasy@gmail.com>
2017-02-04 11:38:40 -0800

    Loop for placeID

--- db/itinerary/itineraryController.js
+++ db/itinerary/itineraryController.js
@@ -9,33 +9,33 @@ const controller = {
     const payload = jwt.verify(token, dbconfig.secret);
     const user = payload.user;
 
-    Itinerary.findOrCreate({
-      where: {
-        itineraryID: req.body.itineraryID
-      },
-      defaults: {
-        password: password
-      }
-    }).spread(function(user, created) {
-      if (created) {
-        console.log('User was successfully created');
-        const token = jwt.sign({user: user.email}, dbconfig.secret, {
-          expiresIn: 86400 // expires in 24 hours
-        });
-
-        return res.json({
-          success: true,
-          token: token
-        });
-      } else {
-        return res.sendStatus(500);
-      }
-    }).catch(function(err) {
-      if (err.original.code === '23505') {
-        return res.status(403).send('That email address already exists, please login');
-      }
-      return res.sendStatus(500);
+    placeIDs.forEach(function(placeID) {
+      console.log(placeID);
     });
+
+    // Itinerary.create({
+    //   itineraryID: req.body.itineraryID,
+    //   password: password
+    // }).spread(function(user, created) {
+    //   if (created) {
+    //     console.log('User was successfully created');
+    //     const token = jwt.sign({user: user.email}, dbconfig.secret, {
+    //       expiresIn: 86400 // expires in 24 hours
+    //     });
+    //
+    //     return res.json({
+    //       success: true,
+    //       token: token
+    //     });
+    //   } else {
+    //     return res.sendStatus(500);
+    //   }
+    // }).catch(function(err) {
+    //   if (err.original.code === '23505') {
+    //     return res.status(403).send('That email address already exists, please login');
+    //   }
+    //   return res.sendStatus(500);
+    // });
 
   },
   retreive: function(req, res, next) {
