d852364d9d095063cbc4a4c059ecf7f0c036eb77
Derek Young <dthomasy@gmail.com>
2017-02-06 16:14:33 -0800

    Prod db credentials

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,25 @@
 const Sequelize = require('sequelize');
-const cfg = require('./dbconfig.js');
 
-const sequelize = new Sequelize(cfg.myLocalDB, cfg.myLocalDBRole, cfg.myLocalDBPW, {
-  host: 'localhost',
+//Testing only -- comment-out for Production
+// const cfg = require('./dbconfig.js');
+//
+// const sequelize = new Sequelize(cfg.myLocalDB, cfg.myLocalDBRole, cfg.myLocalDBPW, {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
+
+//Production only -- comment-out for testing
+const prodDB = 'dboi0jrqlk7thd';
+const prodUser = 'aqpvvsqdgwsioa';
+const prodPW = '4650ae13e0cfcedfba4199739072196672a689d3258c5581b94c4c3383894df7';
+
+const sequelize = new Sequelize(prodDB, prodUser, prodPW, {
+  // host: 'localhost',
   dialect: 'postgres',
   pool: {
     max: 5,
